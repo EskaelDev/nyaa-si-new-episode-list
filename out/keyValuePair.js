7889a0d6d8a593486fd2f6d31138bd4302f08abc
tomek <rainbowvme@gmail.com>
2020-07-17 19:44:37 +0200

    returns anime title with last episode number

---
+++ out/keyValuePair.js
@@ -0,0 +1,1 @@
+//# sourceMappingURL=keyValuePair.js.map
---
+++ out/nyaaResponse.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.NyaaResponse = void 0;
+var NyaaResponse = /** @class */ (function () {
+    function NyaaResponse() {
+    }
+    return NyaaResponse;
+}());
+exports.NyaaResponse = NyaaResponse;
+//# sourceMappingURL=nyaaResponse.js.map
